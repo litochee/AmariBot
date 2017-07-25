0314cc809276e9d1248dd216e8f41ef0476e5bdc
Tyler <litochee@icloud.com>
2017-07-24 23:05:40 -0400

    added level system base

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 const fs = require('fs');
 const sql = require('sqlite');
 const config = require('./config.json');
+const levelerCore = require('./functions/levelSystem');
 
 sql.open(`./db/mainDB.sqlite`);
 
@@ -17,8 +18,10 @@ fs.readdir('./events/', (err, files) => {
 });
 
 client.on("message", message => {
-  if (message.author.bot) return;
+  if (message.author.bot) return; //ignores bots
+  if (message.channel.type !== 'text' || message.channel.type === 'dm') return; //ignores dms
   if (!message.content.startsWith(config.prefix)){//checks if the user is typing in a command or not
+    levelerCore.scoreSystem(message, sql);
 
   }else{
   //splits input to commands
---
+++ functions/levelSystem.js
@@ -0,0 +1,25 @@
+module.exports.scoreSystem = function(message, sql){
+  sql.get(`SELECT * FROM userScores WHERE guildID = '${message.guild.id}' AND userID = '${message.author.id}'`).then(row =>{
+    if (!row){
+      sql.run(`INSERT INTO userScores (guildID, userID, username, globalPoints, nextPL, uLevel, weeklyPoints, globalRank, weeklyRank) VALUES (?,?,?,?,?,?,?,?,?)`, [message.guild.id, message.author.id, message.author.username, 1, 50, 0, 0, 0, 0]);
+    } else{
+      let curPoints = row.globalPoints + 1;
+
+      if (curPoints > row.nextPL){
+        let nPLE = Math.floor(row.nextPL * 1.45);//calculates points for next level
+        sql.run(`UPDATE userScores SET globalPoints=${row.globalPoints + 1}, weeklyPoints=${row.weeklyPoints + 1}, nextPL = ${nPLE}, username='${message.author.username}' WHERE userID=${message.author.id} AND guildID=${message.guild.id}`);
+        message.reply(`Congrats you have leveled up! **${row.uLevel + 1}**! Congrats!`);
+      }//curPoints > row.nextPL
+      //checkRank.levelRank(Message, sql); FOR LEVEL/RANK IMPLEMENTS
+      sql.run(`UPDATE userScores SET globalPoints = ${row.globalPoints + 1}, weeklyPoints = ${row.weeklyPoints + 1} WHERE userID = ${message.author.id} AND guildID = ${message.guild.id}`);//updates points
+      sql.all(`SELECT userID from userScores WHERE guildID = '${message.guild.id}' ORDER BY globalPoints DESC`).then(rColumns =>{
+        const setRankUsers = rColumns.map(z => z.userID);
+        let i = 0;
+        while(setRankUsers[i]){
+          sql.run(`UPDATE userScores SET globalRank = ${i + 1} WHERE userID=${setRankUsers[i]} AND guildID=${message.guild.id}`);
+          i++
+        }//while loop end
+      })
+    }//else !row
+  })//sql
+}
