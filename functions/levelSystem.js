51023b7e0051792222039f21ae25cf3309fd271a
Tyler <litochee@icloud.com>
2017-07-26 22:21:41 -0400

    fixed escaping ' from sqlite
    Fixed the issue with names with special characters not escaping.

--- functions/levelSystem.js
+++ functions/levelSystem.js
@@ -1,6 +1,7 @@
-const jsesc = require('jsesc');
 module.exports.scoreSystem = function(message, sql){
   sql.get(`SELECT * FROM userScores WHERE guildID = '${message.guild.id}' AND userID = '${message.author.id}'`).then(row =>{
+    const eUsername = message.author.username.replace("'", "''");
+    console.log(eUsername);
     if (!row){
       sql.run(`INSERT INTO userScores (guildID, userID, username, globalPoints, nextPL, uLevel, weeklyPoints, globalRank, weeklyRank) VALUES (?,?,?,?,?,?,?,?,?)`, [message.guild.id, message.author.id, message.author.username, 1, 50, 0, 0, 0, 0]);
     } else{
@@ -12,7 +13,7 @@ module.exports.scoreSystem = function(message, sql){
         message.reply(`Congrats you have leveled up! **${row.uLevel + 1}**! Congrats!`);
       }//curPoints > row.nextPL
       //checkRank.levelRank(Message, sql); FOR LEVEL/RANK IMPLEMENTS
-      sql.run(`UPDATE userScores SET globalPoints = ${row.globalPoints + 1}, weeklyPoints = ${row.weeklyPoints + 1} WHERE userID = ${message.author.id} AND guildID = ${message.guild.id}`);//updates points
+      sql.run(`UPDATE userScores SET username='` + eUsername +`', globalPoints = ${row.globalPoints + 1}, weeklyPoints = ${row.weeklyPoints + 1} WHERE userID = ${message.author.id} AND guildID = ${message.guild.id}`);//updates points
       sql.all(`SELECT userID from userScores WHERE guildID = '${message.guild.id}' ORDER BY globalPoints DESC`).then(rColumns =>{
         const setRankUsers = rColumns.map(z => z.userID);
         let i = 0;
